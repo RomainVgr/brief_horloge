959b611800146f5a4bb13f2023ddc5230c43a1e7
zylfu <51331158+zylfu@users.noreply.github.com>
2021-09-19 14:41:39 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 
 
 
-// Déplacer les aiguilles 
+
 function demarrerLaMontre(){
 
-    // Séléctionner les aiguilles de montre
-const AIGUILLEHR = document.querySelector("#hour");
-const AIGUILLEMIN = document.querySelector("#minute");
-const AIGUILLESEC = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
 
@@ -27,15 +23,24 @@ const minutes = now.getMinutes();
 
 const secDegre = ((seconds/60) * 360 + 90);
 
-const minDegre = ((minutes/60) * 360) + ((seconds/60 *6)) + 90;
+const minDegre = ((minutes/60) * 360) + ((seconds/60 *6));
 
-const hourDegre = ((hours /12)*360) + ((minutes/60 * 30)) + 90;
+const hourDegre = ((hours /12)*360) + ((minutes/60 * 30));
+
+
+// Déplacer les aiguilles 
 
     AIGUILLEHR.style.transform = `rotate(${hourDegre}deg)`;
     AIGUILLEMIN.style.transform = `rotate(${minDegre}deg)`;
     AIGUILLESEC.style.transform = `rotate(${secDegre}deg)`;
 
 }
+
+    // Séléctionner les aiguilles de montre
+    const AIGUILLEHR = document.querySelector("#hour");
+    const AIGUILLEMIN = document.querySelector("#minute");
+    const AIGUILLESEC = document.querySelector("#second");
+    
 // Exercuter la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
 

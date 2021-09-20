9d1fa211e63064cbdf4a87dc07840119d366b320
zylfu <51331158+zylfu@users.noreply.github.com>
2021-09-20 16:30:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,48 +1,59 @@
 
-
-
-
 function demarrerLaMontre(){
     
- // Séléctionner les aiguilles de montre
+ // Selectionner les aiguilles de montre
     const AIGUILLEHR = document.querySelector("#hour");
     const AIGUILLEMIN = document.querySelector("#minute");
     const AIGUILLESEC = document.querySelector("#second");
     
 
+//Extraire l'heure actuelle à l'aide de l'objet Date()
 
-//Extraire l'heure actuel à l'aide de l'objet Date()
-
-const now = new Date();
+const date = new Date();
 
 
-//Stocker l'heure , minute , seconde  dans des varaiables
+//Stocker l'heure , minute , seconde  dans des variables
 
-const seconds = now.getSeconds();
-const hours = now.getHours();
-const minutes = now.getMinutes();
+const seconds = date.getSeconds();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+
+// temps en secondes écoulé selon l'heure actuelle extraite avec l'objet date(), et les valeurs extraites 
+// des variables 
+
+const tempsEcouleSec = seconds + (minutes * 60) + (hours * 3600);  
 
 
 
 // Calculer de degré de mouvement de l'aiguille heure, de l'aiguille minute, de l'aiguille seconde
-// Hint : Tous les aiguilles doivent se déplacer chaque seconde selon un degré
 
-const secDegre = ((seconds/60) * 360 + 90);
 
-const minDegre = ((minutes/60) * 360) + ((seconds/60 *6));
+const degSec = 360/60;  // = 6 degrés / sec
+const degMin = 360/60/60; // = 0,1 degré / sec
+const degHeure = 360/12/60/60; //  environ = 0,00833333.../ sec
 
-const hourDegre = ((hours /12)*360) + ((minutes/60 * 30));
 
 
 // Déplacer les aiguilles 
+// Hint : Tous les aiguilles doivent se déplacer chaque seconde selon un degré
 
-    AIGUILLEHR.style.transform = `rotate(${hourDegre}deg)`;
-    AIGUILLEMIN.style.transform = `rotate(${minDegre}deg)`;
-    AIGUILLESEC.style.transform = `rotate(${secDegre}deg)`;
+AIGUILLEHR.style.transform = "rotate(" + tempsEcouleSec * degHeure + "deg";
 
+// On deplace l'aiguille de heures en fonction du temps total de secondes écoulé dans la journée
+// par le degré à ajouter pour chaque seconde ecoulé.
+
+AIGUILLEMIN.style.transform = "rotate(" + tempsEcouleSec * degMin + "deg";
+
+// On deplace l'aiguille de heures en fonction du temps total de secondes écoulé dans la journée
+// par le degré par minute à ajouter pour chaque seconde ecoulé.
+
+AIGUILLESEC.style.transform = "rotate(" + tempsEcouleSec * degSec + "deg";
+
+// On deplace l'aiguille de heures en fonction du temps total de secondes écoulé dans la journée
+// par le degré par seconde  à ajouter pour chaque seconde ecoulé.
 }
 
-
-// Exercuter la fonction chaque second
+// Executer la fonction chaque second
 var interval = setInterval(demarrerLaMontre, 1000);
 
